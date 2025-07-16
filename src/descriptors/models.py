777a87bf9b3cf80dee63df66940dd6b351669601
Drag0nsigh <zantonzazu@gmail.com>
2025-07-16 16:19:30 +0300

    descriptors

--- src/descriptors/models.py
+++ src/descriptors/models.py
@@ -9,4 +9,36 @@ class Model:
 
 
 class Field:
-    pass
+    def __init__(self, path: str):
+        self.path = path
+
+    def _get_value(self, payload: JSON) -> Any:
+        keys = self.path.split('.')
+        data = payload
+        for key in keys:
+            try:
+                data = data[key]
+            except (TypeError, KeyError):
+                return None
+        return data
+
+    def __get__(self, obj: Model, owner: type) -> Any:
+        if obj is None:
+            return None
+        return self._get_value(obj.payload)
+
+    def _set_value(self, payload: JSON, value: Any) -> None:
+        keys = self.path.split('.')
+        data = payload
+        for key in keys[:-1]:
+            try:
+                data = data[key]
+            except (TypeError, KeyError):
+                data[key] = {}
+                data = data[key]
+        data[keys[-1]] = value
+
+    def __set__(self, obj: Model, value: Any) -> None:
+        if obj is None:
+            return
+        self._set_value(obj.payload, value)
